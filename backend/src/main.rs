f1c7c01d620e18fd827cc6c958074741a2e6820a
Buchimi <stormzzy900@gmail.com>
2022-06-17 16:24:46 -0500

    done

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,7 +4,9 @@ extern crate diesel;
 pub mod schema;
 pub mod models;
 
-use std::env;
+use std::{env};
+use std::cell::RefCell;
+use std::collections::LinkedList;
 
 use dotenv::dotenv;
 use diesel::prelude::*;
@@ -13,7 +15,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use models::{User, NewUser};
 
 struct AppState {
-    db_connection: PgConnection,
+    db : RefCell<LinkedList<NewUser>>
 }
 
 #[get("/")]
@@ -24,12 +26,17 @@ async fn hello() -> impl Responder {
 // Creates a user using information from json. TODO - On success will return the unique id of the newly created user
 #[post("/users")]
 async fn create_user(data: web::Data<AppState>, user: web::Json<NewUser>) -> impl Responder {
-    match diesel::insert_into(schema::users::table)
-    .values(user.0)
-    .execute(&data.db_connection) {
-        Ok(_) => HttpResponse::Ok().body(format!("Success. Created user.")),
-        Err(_) => HttpResponse::InternalServerError().body("Failed to create user!"),
-    }
+    let mut stuff = data.db.borrow_mut();
+    stuff.push_front(user.0);
+    println!("{:?}", stuff.front().unwrap());
+    println!("{}", stuff.len());
+    
+    return HttpResponse::Ok().body(format!("Mission complete, size is {}", stuff.len()));
+}
+
+#[get("/users/get")]
+async fn get_users(data: web::Data<AppState>) -> impl Responder{
+    return HttpResponse::Ok().body(data.db.borrow().len().to_string());
 }
 
 #[actix_web::main]
@@ -37,17 +44,13 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     HttpServer::new(|| {
-        let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-        let db_connection = diesel::pg::PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
 
         App::new()
             .app_data(web::Data::new(AppState {
-                db_connection: db_connection,
+                db : RefCell::new(LinkedList::new()),
             }))
             .service(hello)
-            .service(create_user)
+            .service(create_user).service(get_users)
     })
     .bind((
         "127.0.0.1", 
