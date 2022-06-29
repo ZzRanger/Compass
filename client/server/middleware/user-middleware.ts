97aefe6930a335969efaf66d5b8e768aef549b01
ZzRanger <calebmarklim@gmail.com>
2022-06-29 19:07:12 -0400

    initialize nextjs app

--- client/server/middleware/user-middleware.ts
+++
@@ -1,9 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import { useCookie } from 'h3'
-
-export default async (req: IncomingMessage, res: ServerResponse) => {
-  const userCookie = useCookie(req, "userCookie");
-
-  // @ts-ignore
-  req.user = userCookie;
-};
