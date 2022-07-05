e65decad5e11b79d3af98dfda2218d59c47de909
ZzRanger <calebmarklim@gmail.com>
2022-07-05 15:39:45 -0400

    set up firebase auth

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -3,8 +3,22 @@ import type { AppProps } from "next/app";
 import { initFirebase } from "../utils/firebaseUtils";
 import { Provider } from "react-redux";
 import store from "../redux/store";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 initFirebase();
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
