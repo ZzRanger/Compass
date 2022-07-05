8f75e6b3c3d30df641be5973cce4db9bff710048
ZzRanger <calebmarklim@gmail.com>
2022-07-05 16:21:19 -0400

    set up application tracker page layout

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,26 +4,32 @@ import { initFirebase } from "../utils/firebaseUtils";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import NavigationBar from "../components/NavigationBar";
+import { Stack } from "@mui/material";
 
 initFirebase();
 
-const auth = getAuth();
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
+// const auth = getAuth();
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
+//     // User is signed in, see docs for a list of available properties
+//     // https://firebase.google.com/docs/reference/js/firebase.User
+//     const uid = user.uid;
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <Stack>
+        <NavigationBar />
+        <Component {...pageProps} />
+      </Stack>
+
     </Provider>
   );
 }
