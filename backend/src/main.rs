5387a47a419bf66c9992403895736f4ac6e80e79
Buchimi <stormzzy900@gmail.com>
2022-07-06 17:14:52 -0500

    Create PUT request

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,16 +1,19 @@
 #[macro_use]
 extern crate diesel;
 
-pub mod schema;
 pub mod models;
+pub mod schema;
 
 use std::env;
 
+use actix_web::{delete, get, post, put, web, App, HttpResponse, HttpServer, Responder};
+use diesel::prelude::*;
 use dotenv::dotenv;
-use diesel::{prelude::*};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, delete};
 
-use models::{ NewUser};
+use models::{NewUser, User};
+use schema::users;
+
+use crate::schema::users::dsl;
 
 struct AppState {
     db_connection: PgConnection,
@@ -25,51 +28,79 @@ async fn hello() -> impl Responder {
 #[post("/users")]
 async fn create_user(data: web::Data<AppState>, user: web::Json<NewUser>) -> impl Responder {
     println!("{:?}", user.0);
+    create_user_logic(data, user.0)
+}
+
+fn create_user_logic(data: web::Data<AppState>, user: NewUser) -> HttpResponse {
     match diesel::insert_into(schema::users::table)
-    .values(user.0)
-    .execute(&data.db_connection) {
+        .values(user)
+        .execute(&data.db_connection)
+    {
         Ok(_) => HttpResponse::Ok().body(format!("Success. Created user.")),
         Err(_) => HttpResponse::InternalServerError().body("Failed to create user!"),
     }
 }
 
-#[delete("/users/delete/{id}")]
-async fn delete_user(state : web::Data<AppState>, path : web::Path<i32>) -> impl Responder{
+#[put("/users/{id}")]
+async fn update_user(
+    data: web::Data<AppState>,
+    user: web::Json<NewUser>,
+    path: web::Path<i32>,
+) -> impl Responder {
     use schema::users::dsl::*;
-    let user_id = path.into_inner();
-    match diesel::delete(users.filter(id.eq(user_id))).execute(&state.db_connection)
-    {
 
-        Ok(_) => HttpResponse::Ok().body("Delete successful???"),
-        Err(_) => HttpResponse::InternalServerError().body("couldn't delete")
+    let id_number = path.into_inner();
+    let user = user.0.mutate(id_number);
+
+    match diesel::update(users.filter(id.eq(id_number)))
+        .set(&user)
+        .execute(&data.db_connection)
+    {
+        Ok(smth) => {
+            match smth {
+                0 => create_user_logic(data, user.mutate()), //create a new user
+                _ => HttpResponse::Ok().body("User has been updated"),
+            }
+        }
+        Err(_) => HttpResponse::InternalServerError().body("We couldn't update this user"),
     }
 }
+
+#[delete("/users/delete/{id}")]
+async fn delete_user(state: web::Data<AppState>, path: web::Path<i32>) -> impl Responder {
+    use schema::users::dsl::*;
+    let user_id = path.into_inner();
+    match diesel::delete(users.filter(id.eq(user_id))).execute(&state.db_connection) {
+        Ok(_) => HttpResponse::Ok().body("Delete successful???"),
+        Err(_) => HttpResponse::InternalServerError().body("couldn't delete"),
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
     HttpServer::new(|| {
-        let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let db_connection = diesel::pg::PgConnection::establish(&database_url)
-        .expect(&format!("Error connecting to {}", database_url));
+            .expect(&format!("Error connecting to {}", database_url));
 
         App::new()
             .app_data(web::Data::new(AppState {
                 db_connection: db_connection,
             }))
             .service(hello)
-            .service(create_user).
-            service(delete_user)
+            .service(create_user)
+            .service(delete_user)
+            .service(update_user)
     })
     .bind((
-        "127.0.0.1", 
+        "127.0.0.1",
         match env::var("PORT") {
-           Ok(port) => port.parse::<u16>().unwrap(),
-           Err(_) => 8080,
-        }
+            Ok(port) => port.parse::<u16>().unwrap(),
+            Err(_) => 8080,
+        },
     ))?
     .run()
     .await
 }
-
--- backend/src/models.rs
+++ backend/src/models.rs
@@ -1,8 +1,8 @@
-use serde::{Deserialize};
+use serde::Deserialize;
 
 use crate::schema::users;
 
-#[derive(Deserialize, Queryable)]
+#[derive(Deserialize, Queryable, AsChangeset, Debug)]
 pub struct User {
     pub id: i32,
     pub email: Option<String>,
@@ -13,6 +13,19 @@ pub struct User {
     pub major: Option<String>,
 }
 
+impl User {
+    //rename and refactor
+    pub fn mutate(self) -> NewUser {
+        NewUser {
+            email: self.email.unwrap(),
+            first_name: self.first_name.unwrap(),
+            last_name: self.last_name.unwrap(),
+            profile_picture_uri: self.profile_picture_uri,
+            year: self.year.unwrap(),
+            major: self.major.unwrap(),
+        }
+    }
+}
 #[derive(Deserialize, Insertable, Debug)]
 #[table_name = "users"]
 pub struct NewUser {
@@ -22,4 +35,18 @@ pub struct NewUser {
     pub profile_picture_uri: Option<String>,
     pub year: String,
     pub major: String,
+}
+
+impl NewUser {
+    pub fn mutate(self, id: i32) -> User {
+        User {
+            id,
+            email: Some(self.email),
+            first_name: Some(self.first_name),
+            last_name: Some(self.last_name),
+            profile_picture_uri: self.profile_picture_uri,
+            year: Some(self.year),
+            major: Some(self.major),
+        }
+    }
 }
